9b313b29f4e55b28ceecc56320999a79e8f4a844
AkezhanOb1 <esbolatovakezhan@gmail.com>
2019-04-09 18:53:03 +0600

    new problems were solved

--- SortArrayByParityII/main.go
+++ SortArrayByParityII/main.go
@@ -7,6 +7,21 @@ func main() {
 }
 
 func sortArrayByParityII(A []int) []int {
+	wrongOdd := []int{}
+	wrongEven := []int{}
+	for i, v := range A {
+		if i%2 == 0 && v%2 != 0 {
+			wrongOdd = append(wrongOdd, i)
+		}
+		if i%2 != 0 && v%2 == 0 {
+			wrongEven = append(wrongEven, i)
+		}
+	}
 
-	return nil
+	for i := range wrongEven {
+		midVal := A[wrongEven[i]]
+		A[wrongEven[i]] = A[wrongOdd[i]]
+		A[wrongOdd[i]] = midVal
+	}
+	return A
 }
